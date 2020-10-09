69d7cb605d0bcc78c54d359d63df382d0d81757c
Yasunori Horikoshi <yasunori_horikoshi@datawise.co.jp>
2020-10-09 08:09:54 +0000

    setup.sh

--- setup.sh
+++ setup.sh
@@ -19,3 +19,7 @@ sudo systemctl start docker
 
 
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
+     -o /usr/local/bin/docker-compose
+sudo chmod a+x /usr/local/bin/docker-compose
+
