68d655af7dde72391383e0fb843eae0ce14d6df7
Yasunori Horikoshi <yasunori_horikoshi@datawise.co.jp>
2020-10-09 08:08:06 +0000

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo yum remove -y docker \
+     docker-client \
+     docker-client-latest \
+     docker-common \
+     docker-latest \
+     docker-latest-logrotate \
+     docker-logrotate \
+     docker-engine
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+     --add-repo \
+     https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y containerd.io
+sudo yum install -y docker-ce --nobest
+sudo yum install -y docker-ce-cli
+sudo systemctl start docker
+
+
+
